5993d330ab0c887924d822ac8a4ecfaedb3ad74a
natali-alkayed <nataly2000alkayed@gmail.com>
2023-05-02 11:48:24 +0000

    new branch added

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 
 ## Overview- The purpous of this repo is to build a movie app that can check the latest movies based on categories.
 ## Getting Started- In the first step, the server and the database were built for this project.
-## Project Features1. Favourite Movies Page
+
+## Project Features1. Favourite Movies Page/search page/trending/companies page/review page
 
 
 
--- server.js
+++ server.js
@@ -1,44 +1,87 @@
 'use strict';
+require("dotenv").config();
 const express = require("express");
 const data = require('./Movie Data/data.json');
 const app = express();
-const port = 3001;
 const cors = require('cors');
 app.use(cors());
+const axios = require("axios");
+const MoovieKey = process.env.API_KEY;
+const port = process.env.PORT;
 /////////////////////////////////////////////////////
-app.get("/", (req, res) => {
-    let result = [];
-function Movies(title, poster_path, overview) {
-    this.title = title;
-    this.poster_path = poster_path;
-    this.overview = overview;
-    result.push(this);
-}
- let movies=  new Movies(data.title, data.poster_path,data.overview);
-    res.json(movies);
-})
-//////////////////////////////////////////////////
 app.listen(port, () => {
     console.log(`server is listing on port ${port}`);
 });
-//////////////////////////////////////////////////
-app.get('/favorite', favoriteHandler)
-
+////////////////////////////////////////////////////////
+app.get("/", handleHome);
+app.get("/trending", handleTrending);
+app.get("/search", handleSearching);
+app.get('/favorite', favoriteHandler);
+app.get('/companies',handleCompanies);
+app.get('/reviews',handleReviews);
+app.use(notFoundHandler);
+app.use(handleServerError);
+////////////////////////////////////////////////////////
+function Movies(id, title, release_date, poster_path, overview) {
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+///////////////////////////////////////////////////////
+function handleHome(req, res) {
+    let movie = new Movies(data.id, data.title, data.release_date, data.poster_path, data.overview);
+    res.json(movie);
+}
+/////////////////////////////////////////////////////////////////////////////////////////
 function favoriteHandler(req, res) {
-
     res.send('Welcome to Favorite Page');
 }
-/////////////////////////////////////////////////
-app.use(notFoundHandler);
+//******************************//
 function notFoundHandler(req, res) {
     res.status(404).send("page not found error");
 }
-//////////////////////////////////////////////////
 function handleServerError(err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
-  }
+}
+//********************************//
+async function handleTrending(req, res) {
+    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${MoovieKey}&language=en-US`;
 
-  app.use(handleServerError);
-
-
+    let moviesFromAPI = await axios.get(url);
+    let movies = moviesFromAPI.data.results.map((item) => {
+        return new Movies(item.id, item.title, item.release_date, item.poster_path, item.overview);
+    })
+    res.send(movies);
+}
+//**********************************//
+async function handleCompanies (req,res){
+    const companyId = req.query.companyId;
+const url = `https://api.themoviedb.org/3/company/${companyId}?api_key=${MoovieKey}`;
+let companyData = await axios.get(url);
+res.send(companyData.data);
+}
+//*********************************//
+function handleSearching(req, res) {
+    //the query from the frontend
+    const movieName = req.query.name;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${MoovieKey}&language=en-US&query=${movieName}`;
+    axios.get(url)
+        .then((result) => {
+            console.log(result.data);
+            res.send(result.data.results.map((item) => {
+                return new Movies(item.id, item.title, item.release_date, item.poster_path, item.overview);
+            }))
+        })
+}
+//************************************//
+async function handleReviews(req,res){
+    const reviewId = req.query.reviewId;
+    const url = `https://api.themoviedb.org/3/review/${reviewId}?api_key=${MoovieKey}`;
+    let reviewData = await axios.get(url);
+    // let review = new MoviesReview(reviewData.data.id, reviewData.data.author, reviewData.data.url,reviewData.data.rating,reviewData.data.content,reviewData.data.date);
+    res.send(reviewData.data);
+}// try this to test: 58aa82f09251416f92006a3a
+////////////////////////////////////////////////////////////////////////////////////////////////////
